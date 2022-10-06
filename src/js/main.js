578b1f1e8c638e9b0fa57c5d92d66897fd2b2586
Misha <xxxdaddy420@gmail.com>
2022-10-06 14:26:02 +0700

    created burger menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,7 @@
+const headerBurger = document.querySelector('.header__burger'),
+      headerMenu = document.querySelector('.header__menu');
 
+headerBurger.addEventListener('click', () => {
+    headerBurger.classList.toggle('header__burger_active')
+    headerMenu.classList.toggle('header__menu_active')
+})
