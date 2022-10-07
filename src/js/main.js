3709bd755958fd50e8f3fbdd20a6a17938e475ef
Misha <xxxdaddy420@gmail.com>
2022-10-07 11:48:43 +0700

    07.10.22

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,19 @@
 const headerBurger = document.querySelector('.header__burger'),
-      headerMenu = document.querySelector('.header__menu');
+      headerMenu = document.querySelector('.header__menu'),
+      body = document.getElementsByTagName('body')[0];
 
 headerBurger.addEventListener('click', () => {
     headerBurger.classList.toggle('header__burger_active')
     headerMenu.classList.toggle('header__menu_active')
+    body.classList.toggle('hide-scroll')
+    
+})
+
+$('#feedback-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: true,
+    prevArrow: $('.button-prev'),
+    nextArrow: $('.button-next'),
 })
