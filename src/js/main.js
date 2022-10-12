7968d6d1ad729ca6e0383df4ced9f4b6250d7edf
Misha <xxxdaddy420@gmail.com>
2022-10-12 14:43:55 +0700

    vrode listaetsya forma

--- src/js/main.js
+++ src/js/main.js
@@ -17,3 +17,110 @@ $('#feedback-slider').slick({
     prevArrow: $('.button-prev'),
     nextArrow: $('.button-next'),
 })
+
+
+const buttons = document.querySelectorAll('.trigger-form'),
+      form = document.querySelector('.form'),
+      formClose = document.querySelector('.form__close'),
+      formItems = document.querySelectorAll('.form__item'),
+      btnNext = document.querySelectorAll('.form__item-btn'),
+      btnPrev = document.querySelectorAll('.btn-prev');
+
+buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        
+        console.log('click');
+        form.classList.add('form-active')
+        body.classList.add('hide-scroll')
+        showFirstFormItem()
+    })
+})
+formClose.addEventListener('click', () => {
+    form.classList.remove('form-active')
+    body.classList.remove('hide-scroll')
+    formItems.forEach(item => {
+        item.classList.remove('form__item-active')
+        item.classList.add('form__item-non-active')
+    })
+})
+
+function showFirstFormItem () {
+    formItems.forEach(item => {
+       if(item.id !== "0") {
+        item.classList.add('form__item-non-active')
+       } else if(item.id == "0") {
+            item.classList.remove('form__item-non-active')
+            item.classList.add('form__item-active')
+       }
+    })
+}
+
+function formItemActive(idx) {
+    formItems.forEach((item, i) => {
+        if(item.classList.contains('form__item-active')) {
+            item.classList.remove('form__item-active')
+            item.classList.add('form__item-non-active')
+            
+            
+        }
+        if(item.id == idx) {
+            item.classList.add('form__item-active')
+            item.classList.remove('form__item-non-active')
+            
+        }
+})
+}
+
+var idx = 0;
+btnPrev.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        idx-=1
+        console.log(idx);
+        e.preventDefault()
+        // formItems.forEach((item, i) => {
+        //         if(item.classList.contains('form__item-active')) {
+        //             item.classList.remove('form__item-active')
+        //             item.classList.add('form__item-non-active')
+                    
+                    
+        //         }
+        //         if(item.id == idx) {
+        //             item.classList.add('form__item-active')
+        //             item.classList.remove('form__item-non-active')
+                    
+        //         }
+        // })
+        formItemActive(idx)
+    })
+})
+
+
+btnNext.forEach((btn, index) => {
+    
+    btn.addEventListener('click', (e) => {
+        idx+=1
+        e.preventDefault()
+        formItemActive(idx)
+        // formItems.forEach((item, i) => {
+        //         if(item.classList.contains('form__item-active')) {
+        //             item.classList.remove('form__item-active')
+        //             item.classList.add('form__item-non-active')
+                    
+                    
+        //         }
+        //         if(item.id == idx) {
+        //             item.classList.add('form__item-active')
+        //             item.classList.remove('form__item-non-active')
+                    
+        //         }
+        // })
+        // for(let i = 0; i < formItems.length; i++) {
+        //     formItems[i].classList.remove('form__item-active');
+        //     if(i === index) {
+        //         console.log(true);
+        //         console.log(formItems[i]);
+        //         formItems[i].classList.add('form__item-active')
+        //     }
+        // }
+    })
+})
