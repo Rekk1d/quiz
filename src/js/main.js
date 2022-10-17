5de29d9002bcdb0e3cce7d0ba389d2f1f3882a1f
Misha <xxxdaddy420@gmail.com>
2022-10-17 14:44:03 +0700

    test commit before zoom images

--- src/js/main.js
+++ src/js/main.js
@@ -52,22 +52,24 @@ modalOverlay.addEventListener('click', () => {
     }
 })
 
-$("#modal__input").mask("+7 (999) 999-99-99");
+$(document).ready(function() {
+    $("#modal__input").mask("+7 (999) 999-99-99");
     
-$.fn.setCursorPosition = function(pos) {
-    if ($(this).get(0).setSelectionRange) {
-        $(this).get(0).setSelectionRange(pos, pos);
-    } else if ($(this).get(0).createTextRange) {
-        var range = $(this).get(0).createTextRange();
-        range.collapse(true);
-        range.moveEnd('character', pos);
-        range.moveStart('character', pos);
-        range.select();
-    }
-};
-$('#modal__input').click(function(){
-    $(this).setCursorPosition(4); // set position number
-});
+    $.fn.setCursorPosition = function(pos) {
+        if ($(this).get(0).setSelectionRange) {
+            $(this).get(0).setSelectionRange(pos, pos);
+        } else if ($(this).get(0).createTextRange) {
+            var range = $(this).get(0).createTextRange();
+            range.collapse(true);
+            range.moveEnd('character', pos);
+            range.moveStart('character', pos);
+            range.select();
+        }
+    };
+    $('#modal__input').click(function(){
+        $(this).setCursorPosition(4); // set position number
+    });
+})
 
 // modalBtn.addEventListener('click', (e) => {
 //     if(modalInput.value.length < 17) {
